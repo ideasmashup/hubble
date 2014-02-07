9c972025ea6117d035ebbd14d4c79d590bab543a
William ANGER <william@smartificiel.com>
2014-02-07 06:41:40 +0100

    humble : new custom module for all utility function related to BLE from javascript

---
+++ smart_modules/humble.js
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2014 Smartificiel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+/**
+ * <h3>Helpers and Utility functions for BLE</h3>
+ * <p>
+ * This provides tools to allow proper rendering, parsing and processing of BLE
+ * data transmitted to and from devices, classes and functions to handle common
+ * tasks efficiently and quickly.
+ * </p>
+ * <strong>Features:</strong>
+ * <ul>
+ * <li>conversion from binary (or hex) data to javascript values</li>
+ * <li>conversion of javascript values into binary (or hex) data</li>
+ * <li>rendering of characteristics into user-readable values</li>
+ * <li>built-in locale support for localized display of any value</li>
+ * </ul>
+ * 
+ * @author William ANGER <wanger+humble at smartificiel.com>
+ */
+
+// Format-types : https://developer.bluetooth.org/gatt/Pages/FormatTypes.aspx
+var formats = {
+
+};
+
+var units = {
+
+};
+
+var convert = {
+
+};
+
+var characteristics = {
+
+};
+
+exports.convert = convert;
